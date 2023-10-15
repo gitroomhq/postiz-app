5eef382d3d1b13fcbb91faee7f5db96645f21b36
melsonic <madhurjyakalita2017@gmail.com>
2023-10-15 12:31:15 +0530

    added storybook

---
+++ libs/svelte-component/.storybook/main.ts
@@ -0,0 +1,20 @@
+import type { StorybookConfig } from '@storybook/svelte-vite';
+
+const config: StorybookConfig = {
+  stories: ['../src/lib/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
+  addons: ['@storybook/addon-essentials'],
+  framework: {
+    name: '@storybook/svelte-vite',
+    options: {
+      builder: {
+        viteConfigPath: 'libs/svelte-component/vite.config.ts',
+      },
+    },
+  },
+};
+
+export default config;
+
+// To customize your Vite configuration you can use the viteFinal field.
+// Check https://storybook.js.org/docs/react/builders/vite#configuration
+// and https://nx.dev/packages/storybook/documents/custom-builder-configs
