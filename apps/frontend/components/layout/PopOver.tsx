844346fa9cee7699eb58670d2535a03af2f81c3e
Anshu Pathak <78332454+AnshuPathak-88825@users.noreply.github.com>
2023-10-17 12:12:32 +0530

    Update PopOver.tsx removed tailwind and created popover with normal tailwind

--- apps/frontend/components/layout/PopOver.tsx
+++ apps/frontend/components/layout/PopOver.tsx
@@ -1,62 +1,57 @@
-import React, { use } from "react";
-import {
-  Popover,
-  PopoverHandler,
-  PopoverContent,
-  Avatar,
-  Button,
-  Typography,
-  List,
-  ListItem,
-  ListItemPrefix,
-} from "@material-tailwind/react";
+import React from "react";
+import { useState } from "react";
 import { UserFromRequest } from "@clickvote/interfaces";
-type Props = {
-  user: UserFromRequest;
-};
 import PersonIcon from "@mui/icons-material/Person";
 import LogoutIcon from "@mui/icons-material/Logout";
 import { axiosInstance } from "@clickvote/frontend/helper/axios";
 import { useRouter } from "next/router";
+
+type Props = {
+  user: UserFromRequest;
+};
+
 const PopOver: React.FC<Props> = ({ user }) => {
   const router = useRouter();
+  const [isPopoverOpen, setPopoverOpen] = useState(false);
+
   const LogOut = async () => {
     try {
       await axiosInstance.get("/auth/logout");
-      return router.push("/aut/login");
+      router.push("/aut/login");
     } catch (error) {
       console.log("error in logout");
     }
+    setPopoverOpen(false);
   };
+
   return (
-    <div>
-      <Popover placement="bottom">
-        <PopoverHandler>
-          <Button className="rounded-full h-30 w-12 flex items-center justify-center">
-            <PersonIcon />
-          </Button>
-        </PopoverHandler>
-        <PopoverContent className="border border-gray-600	 bg-black	  opacity-0	">
-          <div className="mb-4 flex items-center gap-4 border-b border-blue-gray-50 pb-4">
-            <PersonIcon />
-            <div>
-              <Typography variant="h6" color="blue-gray">
-                {user.email}
-              </Typography>
+    <div className="relative inline-block text-left">
+      <button
+        onClick={() => setPopoverOpen(!isPopoverOpen)}
+        className="rounded-full h-12 w-12 flex items-center justify-center focus:outline-none"
+      >
+        <PersonIcon />
+      </button>
+      {isPopoverOpen && (
+        <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-lg shadow-lg bg-black ring-1 ring-white ring-opacity-5">
+          <div className="py-2">
+            <div className="flex items-center gap-4 border-b border-gray-200 py-2 px-4">
+              <PersonIcon />
+              <div>
+                <h6 className="text-white">{user.email}</h6>
+              </div>
             </div>
-          </div>
-          <List className="p-0">
-            <div onClick={LogOut}>
-              <ListItem>
-                <ListItemPrefix>
+            <ul className="py-2">
+              <li onClick={LogOut} className="px-4 py-2 cursor-pointer ">
+                <div className="flex items-center">
                   <LogoutIcon />
-                </ListItemPrefix>
-                Logout
-              </ListItem>
-            </div>
-          </List>
-        </PopoverContent>
-      </Popover>
+                  <span className="ml-2 text-white">Logout</span>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
