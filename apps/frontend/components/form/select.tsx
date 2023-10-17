0430d22f1cea80537483db351f1c663c2469c09a
badrivlog <bns.love2016@gmail.com>
2023-10-17 23:21:06 +0530

    code formating

--- apps/frontend/components/form/select.tsx
+++ apps/frontend/components/form/select.tsx
@@ -47,7 +47,10 @@ export const Select: FC<
         {...allOther}
         {...all}
         onChange={onChangeFunc}
-        className={clsx(`p-2 w-full bg-[#fff0fd]/10 border border-[#EBECED]/20 rounded-md`, className)}
+        className={clsx(
+          `p-2 w-full bg-[#fff0fd]/10 border border-[#EBECED]/20 rounded-md`,
+          className
+        )}
       >
         {children}
       </select>
