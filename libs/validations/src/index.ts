ee5c2f8903e35d6e9d8a59b4e00e93958621e8a2
prithvi2k2 <prithvi2k2@gmail.com>
2023-10-19 07:28:34 +0530

    Fix : enables `reset` Validators' imports

--- libs/validations/src/index.ts
+++ libs/validations/src/index.ts
@@ -2,3 +2,5 @@ export * from './lib/auth/auth.validator';
 export * from './lib/votes/votes.validation';
 export * from './lib/validations/is.bigger.than';
 export * from './lib/validations/valid.hash';
+export * from './lib/auth/resetRequest.validator'
+export * from './lib/auth/resetConfirm.validator'
