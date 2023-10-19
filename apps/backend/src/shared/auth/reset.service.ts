1a06da7d29930cbc5eb3c8aff6f7a716fe7bdcfb
prithvi2k2 <prithvi2k2@gmail.com>
2023-10-19 07:24:39 +0530

    `reset` service in shared global module

---
+++ apps/backend/src/shared/auth/reset.service.ts
@@ -0,0 +1,37 @@
+import {
+    HttpException,
+    HttpStatus,
+    Injectable,
+  } from '@nestjs/common';
+  import { PasswordResetTokenService } from '@clickvote/backend/src/packages/reset/reset.service';
+  import { UsersService } from '@clickvote/backend/src/packages/users/users.service';
+  import { ResetRequestValidator, ResetConfirmValidator } from '@clickvote/validations';
+  
+  @Injectable()
+  export class ResetPasswordService {
+    constructor(
+      private _resetService: PasswordResetTokenService,
+      private _userService: UsersService
+    ) {}
+  
+    async generateResetToken(reset: ResetRequestValidator) {
+      const user = await this._userService.getByEmail(reset.email);
+      
+      if (!user) {
+        throw new HttpException('User not found', HttpStatus.BAD_REQUEST);
+      }
+      
+      const token = await this._resetService.genResetToken(reset.email);
+  
+      await this._resetService.sendResetLinkToMail(reset.email, token);
+  
+      return {message: 'Reset link sent to mail'};
+    }
+  
+    async setNewPassword(reset: ResetConfirmValidator) {
+      await this._resetService.setPassword(reset.token, reset.password);
+  
+      return {message: 'Password reset successful'}
+    }
+  }
+  
--- apps/backend/src/shared/shared.module.ts
+++ apps/backend/src/shared/shared.module.ts
@@ -2,8 +2,10 @@ import { Global, Module } from '@nestjs/common';
 import { AuthService } from '@clickvote/backend/src/shared/auth/auth.service';
 import { UsersModule } from '@clickvote/backend/src/packages/users/users.module';
 import { RegistrationLoginService } from '@clickvote/backend/src/shared/auth/registration.login.service';
+import { ResetPasswordService } from '@clickvote/backend/src/shared/auth/reset.service';
 import { OrgModule } from '@clickvote/backend/src/packages/org/org.module';
 import { EnvironmentModule } from '@clickvote/backend/src/packages/environment/environment.module';
+import { PasswordResetTokensModule } from '../packages/reset/reset.module';
 import { MongooseModule, RedisService } from '@clickvote/nest-libraries';
 import { MailService } from '@clickvote/backend/src/shared/mail/mail.service';
 import { MailModule } from '@clickvote/backend/src/shared/mail/mail.module';
@@ -15,10 +17,11 @@ import { MailModule } from '@clickvote/backend/src/shared/mail/mail.module';
     UsersModule,
     OrgModule,
     EnvironmentModule,
+    PasswordResetTokensModule,
     MailModule,
   ],
   controllers: [],
-  providers: [AuthService, RedisService, RegistrationLoginService, MailService],
+  providers: [AuthService, RedisService, RegistrationLoginService, ResetPasswordService, MailService],
   get exports() {
     return this.providers;
   },
