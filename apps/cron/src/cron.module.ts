d91aa3edf95211751f8c2cc0c923055e7a76deb2
Nevo David <nevo@notu.co>
2024-03-09 20:14:26 +0700

    feat: fix cron

--- apps/cron/src/cron.module.ts
+++ apps/cron/src/cron.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ScheduleModule } from '@nestjs/schedule';
-import {RefreshTokens} from "@gitroom/cron/tasks/refresh.tokens";
-import {CheckStars} from "@gitroom/cron/tasks/check.stars";
+import { RefreshTokens } from '@gitroom/cron/tasks/refresh.tokens';
+import { CheckStars } from '@gitroom/cron/tasks/check.stars';
+import { DatabaseModule } from '@gitroom/nestjs-libraries/database/prisma/database.module';
+import { RedisModule } from '@gitroom/nestjs-libraries/redis/redis.module';
+import { BullMqModule } from '@gitroom/nestjs-libraries/bull-mq-transport/bull-mq.module';
+import { ioRedis } from '@gitroom/nestjs-libraries/redis/redis.service';
 
 @Module({
-    imports: [ScheduleModule.forRoot()],
-    controllers: [],
-    providers: [RefreshTokens, CheckStars],
+  imports: [
+    DatabaseModule,
+    ScheduleModule.forRoot(),
+    RedisModule,
+    BullMqModule.forRoot({
+      connection: ioRedis,
+    }),
+  ],
+  controllers: [],
+  providers: [RefreshTokens, CheckStars],
 })
 export class CronModule {}
