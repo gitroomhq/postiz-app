c8f51b758a978bbc8e775c123a1c4c529df94ad3
Nevo David <nevo@notu.co>
2024-03-12 00:17:57 +0700

    feat: modal

--- apps/frontend/src/components/launches/calendar.tsx
+++ apps/frontend/src/components/launches/calendar.tsx
@@ -265,7 +265,7 @@ const CalendarColumnRender: FC<{ day: number; hour: string }> = (props) => {
         closeOnEscape: false,
         withCloseButton: false,
         classNames: {
-          modal: 'bg-transparent text-white !w-[auto]',
+          modal: 'bg-transparent text-white',
         },
         children: (
           <ExistingDataContextProvider value={data}>
@@ -277,7 +277,7 @@ const CalendarColumnRender: FC<{ day: number; hour: string }> = (props) => {
             />
           </ExistingDataContextProvider>
         ),
-        size: 'dynamic',
+        size: '80%',
         title: ``,
       });
     },
@@ -293,7 +293,7 @@ const CalendarColumnRender: FC<{ day: number; hour: string }> = (props) => {
         modal: 'bg-transparent text-white',
       },
       children: <AddEditModal integrations={integrations} date={getDate} />,
-      size: '80%'
+      size: '80%',
       // title: `Adding posts for ${getDate.format('DD/MM/YYYY HH:mm')}`,
     });
   }, []);
