ad10b4b329c2c7aa2b13251597cf9b561807e492
Nevo David <nevo@notu.co>
2024-03-19 11:20:09 +0700

    feat: ignore api key

--- libraries/nestjs-libraries/src/services/email.service.ts
+++ libraries/nestjs-libraries/src/services/email.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.RESEND_API_KEY || 're_132');
 
 @Injectable()
 export class EmailService {
   async sendEmail(to: string, subject: string, html: string) {
+    if (!process.env.RESEND_API_KEY) {
+      console.log('No Resend API Key found, skipping email sending');
+      return;
+    }
     await resend.emails.send({
       from: 'Gitroom <no-reply@gitroom.com>',
       to,
