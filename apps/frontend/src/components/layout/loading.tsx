a93af6d9f214207ee258d40c5df4dcea0fe33abe
Nevo David <nevo@notu.co>
2024-05-14 18:38:05 +0700

    feat: deployment

--- apps/frontend/src/components/layout/loading.tsx
+++ apps/frontend/src/components/layout/loading.tsx
@@ -4,3 +4,4 @@ import ReactLoading from 'react-loading';
 export const LoadingComponent = () => {
   return <div className="flex-1 flex justify-center pt-[100px]"><ReactLoading type="spin" color="#fff" width={100} height={100} /></div>;
 };
+
