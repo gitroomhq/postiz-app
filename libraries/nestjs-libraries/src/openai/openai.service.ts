43b13b8b42992bb04dee4e845541edd3792e4c19
Nevo David <nevo@notu.co>
2024-05-24 19:22:07 +0700

    feat: openai fix

--- libraries/nestjs-libraries/src/openai/openai.service.ts
+++ libraries/nestjs-libraries/src/openai/openai.service.ts
@@ -3,7 +3,7 @@ import OpenAI from 'openai';
 import { shuffle } from 'lodash';
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.OPENAI_API_KEY || 'sk-proj-',
 });
 
 @Injectable()
