e721a791325cc1ef6b9e4139b92e2bf3e40ea49c
Nevo David <nevo@notu.co>
2024-08-10 12:35:10 +0700

    feat: cheaper model

--- apps/backend/src/api/routes/copilot.controller.ts
+++ apps/backend/src/api/routes/copilot.controller.ts
@@ -22,7 +22,7 @@ export class CopilotController {
           req?.body?.variables?.data?.metadata?.requestType ===
           'TextareaCompletion'
             ? 'gpt-4o-mini'
-            : 'gpt-4o',
+            : 'gpt-4o-2024-08-06',
       }),
     });
 
