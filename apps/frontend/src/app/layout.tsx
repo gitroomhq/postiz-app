72e8ef868ff2245294c4a3938127d31bcd3e1f16
Nevo David <nevo@notu.co>
2024-09-11 00:17:30 +0700

    feat: font change

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -3,12 +3,15 @@ export const dynamic = 'force-dynamic';
 import './global.scss';
 import 'react-tooltip/dist/react-tooltip.css';
 import '@copilotkit/react-ui/styles.css';
+
 import LayoutContext from '@gitroom/frontend/components/layout/layout.context';
 import { ReactNode } from 'react';
+import { Chakra_Petch } from 'next/font/google';
 import { isGeneral } from '@gitroom/react/helpers/is.general';
 import PlausibleProvider from 'next-plausible';
 import clsx from 'clsx';
-import "@fontsource/chakra-petch";
+
+const chakra = Chakra_Petch({ weight: '400', subsets: ['latin'] });
 
 export default async function AppLayout({ children }: { children: ReactNode }) {
   return (
@@ -20,7 +23,7 @@ export default async function AppLayout({ children }: { children: ReactNode }) {
           sizes="any"
         />
       </head>
-      <body className={clsx('chakra-petch', 'text-primary dark')}>
+      <body className={clsx(chakra.className, 'text-primary dark')}>
         <PlausibleProvider domain={isGeneral() ? "postiz.com" : "gitroom.com"}>
           <LayoutContext>{children}</LayoutContext>
         </PlausibleProvider>
