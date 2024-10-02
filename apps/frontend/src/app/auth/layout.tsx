4121cdc28b1753e0ff5a1a1d06fc58d75103731f
GURSAL PRATHAMESH APPASAHEB <135983263+prathamesh424@users.noreply.github.com>
2024-10-02 17:45:03 +0530

    Update layout.tsx

--- apps/frontend/src/app/auth/layout.tsx
+++ apps/frontend/src/app/auth/layout.tsx
@@ -5,6 +5,7 @@ import Image from 'next/image';
 import clsx from 'clsx';
 import loadDynamic from 'next/dynamic';
 import { isGeneralServerSide } from '@gitroom/helpers/utils/is.general.server.side';
+import React from 'react';
 const ReturnUrlComponent = loadDynamic(() => import('./return.url.component'));
 
 export default async function AuthLayout({
@@ -17,7 +18,7 @@ export default async function AuthLayout({
       <ReturnUrlComponent />
       <div className="absolute left-0 top-0 z-[0] h-[100vh] w-[100vw] overflow-hidden bg-loginBg bg-contain bg-no-repeat bg-left-top" />
       <div className="relative z-[1] px-3 lg:pr-[100px] xs:mt-[70px] flex justify-center lg:justify-end items-center h-[100vh] w-[100vw] overflow-hidden">
-        <div className="w-full max-w-lg h-[614px] flex flex-col bg-loginBox bg-no-repeat bg-contain">
+        <div className="w-full max-w-lg h-[614px] flex flex-col bg-loginBox bg-no-repeat bg-contain ">
           <div className="w-full relative">
             <div className="custom:fixed custom:text-left custom:left-[20px] custom:justify-start custom:top-[20px] absolute -top-[100px] text-textColor justify-center items-center w-full flex gap-[10px]">
               <Image
@@ -60,8 +61,8 @@ export default async function AuthLayout({
               </div>
             </div>
           </div>
-          <div className="p-[32px] w-full h-[614px] text-textColor">
-            {children}
+          <div className="p-[30px] w-full h-[614px] text-textColor">
+                {children}
           </div>
           <div className="flex flex-1 flex-col">
             <div className="flex-1 flex justify-end">
