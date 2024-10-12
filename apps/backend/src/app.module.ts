d3a5c97c0e1db0a4835c286ad2e9e14d173cc686
Nevo David <nevo@notu.co>
2024-10-12 22:14:34 +0700

    feat: check plugins

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,21 +1,24 @@
-import {Global, Module} from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 
-import {DatabaseModule} from "@gitroom/nestjs-libraries/database/prisma/database.module";
-import {ApiModule} from "@gitroom/backend/api/api.module";
-import {APP_GUARD} from "@nestjs/core";
-import {PoliciesGuard} from "@gitroom/backend/services/auth/permissions/permissions.guard";
+import { DatabaseModule } from '@gitroom/nestjs-libraries/database/prisma/database.module';
+import { ApiModule } from '@gitroom/backend/api/api.module';
+import { APP_GUARD } from '@nestjs/core';
+import { PoliciesGuard } from '@gitroom/backend/services/auth/permissions/permissions.guard';
 import { BullMqModule } from '@gitroom/nestjs-libraries/bull-mq-transport-new/bull.mq.module';
+import { PluginModule } from '@gitroom/plugins/plugin.module';
 
 @Global()
 @Module({
-  imports: [BullMqModule, DatabaseModule, ApiModule],
+  imports: [BullMqModule, DatabaseModule, ApiModule, PluginModule],
   controllers: [],
-  providers: [{
-    provide: APP_GUARD,
-    useClass: PoliciesGuard
-  }],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: PoliciesGuard,
+    },
+  ],
   get exports() {
     return [...this.imports];
-  }
+  },
 })
 export class AppModule {}
