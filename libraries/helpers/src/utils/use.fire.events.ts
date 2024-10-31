8a4a397d0a7dbdcae30ba75346235bbd3ae4b6e5
Nevo David <me@nevos.io>
2024-10-31 11:53:24 +0700

    feat: variables

--- libraries/helpers/src/utils/use.fire.events.ts
+++ libraries/helpers/src/utils/use.fire.events.ts
@@ -1,19 +1,28 @@
 import { usePlausible } from 'next-plausible';
 import { useCallback } from 'react';
-import { useVariables } from '@gitroom/react/helpers/variable.context';
 import { usePostHog } from 'posthog-js/react';
+import { useVariables } from '@gitroom/react/helpers/variable.context';
+import { useUser } from '@gitroom/frontend/components/layout/user.context';
 
 export const useFireEvents = () => {
   const { billingEnabled } = useVariables();
   const plausible = usePlausible();
   const posthog = usePostHog();
+  const user = useUser();
 
-  return useCallback((name: string, props?: any) => {
-    if (!billingEnabled) {
-      return;
-    }
+  return useCallback(
+    (name: string, props?: any) => {
+      if (!billingEnabled) {
+        return;
+      }
 
-    posthog.capture(name, props);
-    plausible(name, { props });
-  }, []);
+      if (user) {
+        posthog.identify(user.id, { email: user.email, name: user.name });
+      }
+
+      posthog.capture(name, props);
+      plausible(name, { props });
+    },
+    [user]
+  );
 };
