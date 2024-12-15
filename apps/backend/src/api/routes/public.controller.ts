287f4040b49fd65fb224222b8c9abaedd9d26e4a
Nevo David <me@nevos.io>
2024-12-15 13:17:10 +0700

    feat: preview

--- apps/backend/src/api/routes/public.controller.ts
+++ apps/backend/src/api/routes/public.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { AgenciesService } from '@gitroom/nestjs-libraries/database/prisma/agencies/agencies.service';
+import { PostsService } from '@gitroom/nestjs-libraries/database/prisma/posts/posts.service';
 
 @ApiTags('Public')
 @Controller('/public')
 export class PublicController {
-  constructor(private _agenciesService: AgenciesService) {}
+  constructor(
+    private _agenciesService: AgenciesService,
+    private _postsService: PostsService
+  ) {}
   @Get('/agencies-list')
   async getAgencyByUser() {
     return this._agenciesService.getAllAgencies();
@@ -17,14 +21,32 @@ export class PublicController {
   }
 
   @Get('/agencies-information/:agency')
-  async getAgencyInformation(
-    @Param('agency') agency: string,
-  ) {
+  async getAgencyInformation(@Param('agency') agency: string) {
     return this._agenciesService.getAgencyInformation(agency);
   }
 
   @Get('/agencies-list-count')
   async getAgenciesCount() {
     return this._agenciesService.getCount();
+  }
+
+  @Get(`/posts/:id`)
+  async getPreview(@Param('id') id: string) {
+    return (await this._postsService.getPostsRecursively(id, true)).map(
+      ({ childrenPost, ...p }) => ({
+        ...p,
+        ...(p.integration
+          ? {
+              integration: {
+                id: p.integration.id,
+                name: p.integration.name,
+                picture: p.integration.picture,
+                providerIdentifier: p.integration.providerIdentifier,
+                profile: p.integration.profile,
+              },
+            }
+          : {}),
+      })
+    );
   }
 }
---
+++ apps/frontend/src/app/(preview)/p/[id]/layout.tsx
@@ -0,0 +1,5 @@
+import { ReactNode } from 'react';
+
+export default async function AppLayout({ children }: { children: ReactNode }) {
+  return children;
+}
