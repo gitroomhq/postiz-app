95864a8af74b29e01897187a7c285ad3bb1d51e3
Nevo David <me@nevos.io>
2025-01-07 00:36:14 +0700

    feat: fix

--- apps/frontend/src/components/launches/polonto.tsx
+++ apps/frontend/src/components/launches/polonto.tsx
@@ -90,7 +90,7 @@ const Polonto: FC<{
     };
   }, []);
   return (
-    <div className="fixed left-0 top-0 bg-primary/80 z-[300] w-full min-h-full p-[60px] animate-fade">
+    <div className="fixed left-0 top-0 bg-primary/80 z-[300] w-full min-h-full px-[60px] animate-fade">
       <div className="w-full h-full bg-sixth border-tableBorder border-2 rounded-xl pb-[20px] px-[20px] relative">
         <div className="flex">
           <div className="flex-1">
@@ -121,7 +121,7 @@ const Polonto: FC<{
           <CloseContext.Provider
             value={{ close: () => closeModal(), setMedia }}
           >
-            <PolotnoContainer style={{ width: '100%', height: '1000px' }}>
+            <PolotnoContainer style={{ width: '100%', height: '700px' }}>
               <SidePanelWrap>
                 <SidePanel store={store} sections={features} />
               </SidePanelWrap>
