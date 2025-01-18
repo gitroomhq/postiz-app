ad1e5e71e2815e0288f873ef4b8e9b87a8432327
Will <williambernardo838@gmail.com>
2025-01-17 22:43:08 -0300

    tests: Adicionando ambiente para execucao dos testes

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,10 @@
 import { getJestProjects } from '@nx/jest';
 
-export default {
-  projects: getJestProjects(),
+module.exports = {
+  preset: 'ts-jest',  // Usando ts-jest para suportar TypeScript
+  testEnvironment: 'node',  // Ambiente de teste para Node.js
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  testMatch: ['**/tests/**/*.spec.ts', '**/tests/**/*.test.ts'],  // Caminho para os testes
+  testPathIgnorePatterns: ['/node_modules/'],  // Ignora a pasta node_modules
+  verbose: true,  // Exibe detalhes adicionais ao executar os testes
 };
