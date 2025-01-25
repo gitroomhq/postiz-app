a02806fadb8fbbc9dc890d33a9744dae28cc6df2
Nevo David <me@nevos.io>
2025-01-25 22:29:15 +0700

    feat: nostr

---
+++ apps/frontend/src/components/launches/providers/nostr/nostr.provider.tsx
@@ -0,0 +1,11 @@
+import { withProvider } from '@gitroom/frontend/components/launches/providers/high.order.provider';
+
+export default withProvider(
+  null,
+  undefined,
+  undefined,
+  async () => {
+    return true;
+  },
+  undefined,
+);
