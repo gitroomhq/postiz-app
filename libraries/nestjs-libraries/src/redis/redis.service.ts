54a3e943c07af964dd6d4b9f81126d492efbdd99
Chad $yntax <chad@ttpspodcast.com>
2025-02-15 20:50:43 -0500

    add log

--- libraries/nestjs-libraries/src/redis/redis.service.ts
+++ libraries/nestjs-libraries/src/redis/redis.service.ts
@@ -1,6 +1,8 @@
 import { Redis } from 'ioredis';
 
+console.log(`Connecting to Redis at ${process.env.REDIS_URL}!`);
+
 export const ioRedis = new Redis(process.env.REDIS_URL!, {
   maxRetriesPerRequest: null,
-  connectTimeout: 10000
+  connectTimeout: 10000,
 });
