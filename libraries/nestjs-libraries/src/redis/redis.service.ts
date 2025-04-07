b740c5a0c3e1a55fdfefdd686c97526da81869e3
EDISON <edisonpadilla.dev@gmail.com>
2025-04-07 13:54:51 -0400

    fix: redis conection

--- libraries/nestjs-libraries/src/redis/redis.service.ts
+++ libraries/nestjs-libraries/src/redis/redis.service.ts
@@ -1,6 +1,6 @@
 import { Redis } from 'ioredis';
 
-export const ioRedis = new Redis(process.env.REDIS_URL!, {
+export const ioRedis = new Redis(process.env.REDIS_URL + '?family=0', {
   maxRetriesPerRequest: null,
   connectTimeout: 10000
 });
