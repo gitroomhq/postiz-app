935ea1cb54f0b4410f70cb95a801418b77c0a6b9
Nevo David <me@nevos.io>
2025-05-03 14:05:00 +0700

    feat: go back to dall-e

--- libraries/nestjs-libraries/src/agent/agent.graph.service.ts
+++ libraries/nestjs-libraries/src/agent/agent.graph.service.ts
@@ -27,7 +27,7 @@ const model = new ChatOpenAI({
 
 const dalle = new DallEAPIWrapper({
   apiKey: process.env.OPENAI_API_KEY || 'sk-proj-',
-  model: 'gpt-image-1',
+  model: 'dall-e-3',
 });
 
 interface WorkflowChannelsState {
