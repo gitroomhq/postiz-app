09f79977e103a09ad0977498efc8cd4490746cef
egelhaus <156946629+egelhaus@users.noreply.github.com>
2025-05-16 17:57:33 +0200

    Update youtube.provider.ts

--- apps/extension/src/providers/list/youtube.provider.ts
+++ apps/extension/src/providers/list/youtube.provider.ts
@@ -2,13 +2,12 @@ import { ProviderInterface } from "@gitroom/extension/providers/provider.interfa
 
 export class YoutubeProvider implements ProviderInterface {
   identifier = "youtube";
-  baseUrl = "https://youtube.com";
-  element = `tp-yt-paper-item[test-id="upload-beta"]`;
-  attachTo = `#container.ytd-masthead`;
+  baseUrl = "https://www.youtube.com";
+  element = `ytd-topbar-menu-button-renderer button[aria-label^="Create"]`;
+  attachTo = `#end`;
   style = "dark" as "dark";
 
   findIdentifier = (element: HTMLElement) => {
-    // Extracts video ID from URL if available, else fallback to the full URL
     const match = window.location.href.match(/v=([\w-]{11})/);
     return match ? match[1] : window.location.href;
   };
