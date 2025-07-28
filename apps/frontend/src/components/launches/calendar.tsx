7fba75dbc74606ecd12623c6851bf722548fb90a
Nevo David <me@nevos.io>
2025-07-28 20:55:24 +0700

    feat: no content

--- apps/frontend/src/components/launches/calendar.tsx
+++ apps/frontend/src/components/launches/calendar.tsx
@@ -932,7 +932,7 @@ const CalendarItem: FC<{
           </div>
           <div className="w-full relative">
             <div className="absolute top-0 start-0 w-full text-ellipsis break-words line-clamp-1 text-left">
-              {stripHtmlValidation('none', post.content, false, true)}
+              {stripHtmlValidation('none', post.content, false, true) || 'no content'}
             </div>
           </div>
         </div>
