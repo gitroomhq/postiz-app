a67f6b3918cece042fff0ab11c3856eb7c0d0d2d
Nevo David <me@nevos.io>
2025-08-02 12:05:24 +0700

    feat: client revert

--- libraries/react-shared-libraries/src/sentry/initialize.sentry.client.ts
+++ libraries/react-shared-libraries/src/sentry/initialize.sentry.client.ts
@@ -1,11 +1,6 @@
 import * as Sentry from '@sentry/nextjs';
 import { initializeSentryBasic } from '@gitroom/react/sentry/initialize.sentry.next.basic';
 
-// Import or define frontendUrl, backendUrl and internalBackendUrl
-const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:4200';
-const backendUrl = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3000';
-const internalBackendUrl = process.env.BACKEND_INTERNAL_URL || 'http://localhost:3000';
-
 export const initializeSentryClient = (environment: string, dsn: string) =>
   initializeSentryBasic(environment, dsn, {
     integrations: [
@@ -14,15 +9,6 @@ export const initializeSentryClient = (environment: string, dsn: string) =>
       Sentry.replayIntegration({
         maskAllText: true,
         maskAllInputs: true,
-
-        // Allow (Internal) API and Frontend requests to be captured
-      networkDetailAllowUrls: [
-        new RegExp(`^${frontendUrl}(?:/.*)?$`),
-        new RegExp(`^${backendUrl}(?:/.*)?$`),
-        new RegExp(`^${internalBackendUrl}(?:/.*)?$`)
-      ],
-      networkRequestHeaders: ['X-Custom-Header'],
-      networkResponseHeaders: ['X-Custom-Header'],
       }),
       Sentry.feedbackIntegration({
         // Disable the injection of the default widget
