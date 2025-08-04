f38e533ba014961e992797266a584a728a9b5e9e
Yonggan <yonggan@obco.pro>
2025-08-04 22:46:43 +0200

    Thanks to @aumetra to fix the shitty crypto

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A prisma test project";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        nativeBuildInputs = [ pkgs.bashInteractive ];
+        buildInputs = with pkgs; [
+          nodePackages.prisma
+          nodePackages.npm
+          nodejs-slim
+        ];
+        shellHook = with pkgs; ''
+          export PRISMA_SCHEMA_ENGINE_BINARY="${prisma-engines}/bin/schema-engine"
+          export PRISMA_QUERY_ENGINE_BINARY="${prisma-engines}/bin/query-engine"
+          export PRISMA_QUERY_ENGINE_LIBRARY="${prisma-engines}/lib/libquery_engine.node"
+          export PRISMA_FMT_BINARY="${prisma-engines}/bin/prisma-fmt"
+        '';
+      };
+    });
+}
