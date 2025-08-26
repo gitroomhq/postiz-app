3bea9ee7e9886f2155a8449c7e45219a44cae9bb
Nevo David <me@nevos.io>
2025-08-26 23:12:56 +0700

    feat: disconnect prisma

--- apps/frontend/src/components/preview/render.preview.date.tsx
+++ apps/frontend/src/components/preview/render.preview.date.tsx
@@ -4,5 +4,6 @@ import utc from 'dayjs/plugin/utc';
 dayjs.extend(utc);
 
 export const RenderPreviewDate: FC<{ date: string }> = ({ date }) => {
+  console.log(date);
   return <>{dayjs.utc(date).local().format('MMMM D, YYYY h:mm A')}</>;
 };
