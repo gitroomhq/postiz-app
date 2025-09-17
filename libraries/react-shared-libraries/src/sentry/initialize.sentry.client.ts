9eb68484ffaab37c6156c90d0d96f31f9c65b23d
Enno Gelhaus <egelhaus@ennogelhaus.de>
2025-09-17 21:38:58 +0200

    Remove node profiling integration from Sentry client
    Removed node profiling integration and related settings.

--- libraries/react-shared-libraries/src/sentry/initialize.sentry.client.ts
+++ libraries/react-shared-libraries/src/sentry/initialize.sentry.client.ts
@@ -1,13 +1,11 @@
 import * as Sentry from '@sentry/nextjs';
 import { initializeSentryBasic } from '@gitroom/react/sentry/initialize.sentry.next.basic';
-import { nodeProfilingIntegration } from '@sentry/profiling-node';
 
 export const initializeSentryClient = (environment: string, dsn: string) =>
   initializeSentryBasic(environment, dsn, {
     integrations: [
       // Add default integrations back
       Sentry.browserTracingIntegration(),
-      nodeProfilingIntegration(),
       Sentry.replayIntegration({
         maskAllText: true,
         maskAllInputs: true,
@@ -19,9 +17,4 @@ export const initializeSentryClient = (environment: string, dsn: string) =>
     ],
     replaysSessionSampleRate: environment === 'development' ? 1.0 : 0.5,
     replaysOnErrorSampleRate: 1.0,
-
-        
-     // Profiling
-    profileSessionSampleRate: process.env.NODE_ENV === 'development' ? 1.0 : 0.15,
-    profileLifecycle: 'trace',
   });
