5f4de7c987144b050d89734fb886fbe5118f2e42
Nevo David <me@nevos.io>
2025-12-29 22:20:16 +0700

    feat: fullscreen

--- apps/frontend/src/app/(extension)/modal/[style]/[platform]/page.tsx
+++ apps/frontend/src/app/(extension)/modal/[style]/[platform]/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import { StandaloneModal } from '@gitroom/frontend/components/standalone-modal/standalone.modal';
-import { usePathname } from 'next/navigation';
 export default async function Modal() {
   return (
-    <div className="text-textColor h-screen w-screen">
-      <StandaloneModal />
+    <div className="w-screen h-screen overflow-hidden bg-black">
+      <div className="text-textColor h-[calc(100vh+80px)] w-[calc(100vw+80px)] -m-[40px]">
+        <StandaloneModal />
+      </div>
     </div>
   );
 }
