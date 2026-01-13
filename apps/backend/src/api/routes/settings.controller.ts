f30d04701494098a5bdb2d78e9083c257fa0e894
DominikLudwiczak <dominik.ludwiczak.priv@gmail.com>
2026-01-13 08:24:31 +0100

    fix User model import

--- apps/backend/src/api/routes/settings.controller.ts
+++ apps/backend/src/api/routes/settings.controller.ts
@@ -1,12 +1,11 @@
 import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { GetOrgFromRequest } from '@gitroom/nestjs-libraries/user/org.from.request';
-import { Organization } from '@prisma/client';
+import { Organization, User } from '@prisma/client';
 import { CheckPolicies } from '@gitroom/backend/services/auth/permissions/permissions.ability';
 import { OrganizationService } from '@gitroom/nestjs-libraries/database/prisma/organizations/organization.service';
 import { AddTeamMemberDto } from '@gitroom/nestjs-libraries/dtos/settings/add.team.member.dto';
 import { ApiTags } from '@nestjs/swagger';
 import { AuthorizationActions, Sections } from '@gitroom/backend/services/auth/permissions/permission.exception.class';
-import { User } from 'facebook-nodejs-business-sdk';
 import { GetUserFromRequest } from '@gitroom/nestjs-libraries/user/user.from.request';
 import { UsersService } from '@gitroom/nestjs-libraries/database/prisma/users/users.service';
 import { ChangePasswordDto } from '@gitroom/nestjs-libraries/dtos/settings/change.password.dto';
