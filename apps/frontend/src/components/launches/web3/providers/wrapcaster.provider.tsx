604b029a9d7980199016543d381bfc67b93993c0
Nevo David <me@nevos.io>
2026-01-18 09:44:22 +0700

    feat: start button

--- apps/frontend/src/components/launches/web3/providers/wrapcaster.provider.tsx
+++ apps/frontend/src/components/launches/web3/providers/wrapcaster.provider.tsx
@@ -34,6 +34,7 @@ export const WrapcasterProvider: FC<Web3ProviderInterface> = (props) => {
         </div>
       ) : (
         <div className="justify-center items-center py-[20px] flex-col w-[500px]">
+          <div>Click on the bottom below to start the process</div>
           <ButtonCaster login={auth} />
         </div>
       )}
