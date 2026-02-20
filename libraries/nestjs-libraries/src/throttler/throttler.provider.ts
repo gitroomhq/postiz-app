7627b80bfcafe82ffe3c9211f2a3297004c431fa
Nevo David <me@nevos.io>
2026-02-20 17:53:13 +0700

    feat: remove upload throttle

--- libraries/nestjs-libraries/src/throttler/throttler.provider.ts
+++ libraries/nestjs-libraries/src/throttler/throttler.provider.ts
@@ -8,10 +8,7 @@ export class ThrottlerBehindProxyGuard extends ThrottlerGuard {
     context: ExecutionContext
   ): Promise<boolean> {
     const { url, method } = context.switchToHttp().getRequest<Request>();
-    if (
-      method === 'POST' &&
-      (url.includes('/public/v1/posts') || url.includes('/public/v1/upload'))
-    ) {
+    if (method === 'POST' && url.includes('/public/v1/posts')) {
       return super.canActivate(context);
     }
 
