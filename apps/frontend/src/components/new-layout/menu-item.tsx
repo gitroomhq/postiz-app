fe51c788c70d3d10cca86bde5b38a2bbfa54122f
Nevo David <me@nevos.io>
2026-03-04 15:20:58 +0700

    feat: agent media

--- apps/frontend/src/components/new-layout/menu-item.tsx
+++ apps/frontend/src/components/new-layout/menu-item.tsx
@@ -16,6 +16,7 @@ export const MenuItem: FC<{ label: string; icon: ReactNode; path: string }> = ({
     <Link
       prefetch={true}
       href={path}
+      {...path.indexOf('http') === 0 && { target: '_blank' }}
       className={clsx(
         'w-full minCustom:h-[54px] custom:h-[30px] py-[8px] px-[6px] gap-[4px] flex flex-col custom:flex-row text-[10px] font-[600] items-center minCustom:justify-center rounded-[12px] hover:text-textItemFocused hover:bg-boxFocused',
         isActive ? 'text-textItemFocused bg-boxFocused' : 'text-textItemBlur'
