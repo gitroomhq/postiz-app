42d2d363a6922ad644437cd5515f867993b86487
Krzysztof Rzepka <krisrz@mac.home>
2026-03-14 20:29:53 +0000

    ci: remove nx dependency from jest config

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,11 @@
-import { getJestProjects } from '@nx/jest';
-
 export default {
-  projects: getJestProjects(),
+  roots: ['<rootDir>/apps', '<rootDir>/libraries'],
+  testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  testEnvironment: 'node',
+  collectCoverageFrom: [
+    'apps/**/*.{ts,tsx,js,jsx}',
+    'libraries/**/*.{ts,tsx,js,jsx}',
+    '!**/*.d.ts',
+  ],
 };
