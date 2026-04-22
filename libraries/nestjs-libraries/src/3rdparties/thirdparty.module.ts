73ebfc3273b20fb0dd719290ff04d6752c78e505
Burak Bayır <burakbayir1@gmail.com>
2026-04-23 01:27:57 +0300

    feat: register xquik provider in thirdparty module

--- libraries/nestjs-libraries/src/3rdparties/thirdparty.module.ts
+++ libraries/nestjs-libraries/src/3rdparties/thirdparty.module.ts
@@ -1,11 +1,12 @@
 import { Global, Module } from '@nestjs/common';
 import { HeygenProvider } from '@gitroom/nestjs-libraries/3rdparties/heygen/heygen.provider';
 import { ReelFarmProvider } from '@gitroom/nestjs-libraries/3rdparties/reelfarm/reelfarm.provider';
+import { XquikProvider } from '@gitroom/nestjs-libraries/3rdparties/xquik/xquik.provider';
 import { ThirdPartyManager } from '@gitroom/nestjs-libraries/3rdparties/thirdparty.manager';
 
 @Global()
 @Module({
-  providers: [HeygenProvider, ReelFarmProvider, ThirdPartyManager],
+  providers: [HeygenProvider, ReelFarmProvider, XquikProvider, ThirdPartyManager],
   get exports() {
     return this.providers;
   },
